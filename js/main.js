8a2e4b8395d7fed9c354e26ebb01ec5020f05416
Nayeem <green.habib@gmail.com>
2021-11-19 22:34:46 +0600

    completed

---
+++ js/main.js
@@ -0,0 +1,75 @@
+let isCouponCodeApplied = false;
+
+// Declare function to calculate product price
+function productPrice(product, price){
+    const productCost = document.getElementById(product + '-cost')
+    productCost.innerText = price;
+    calculateTotal()
+}
+
+// To calculate and update total price
+function calculateTotal(){
+    // To catch Element from html
+    const memoryCost = parseFloat(document.getElementById('memory-cost').innerText);
+    const storageCost = parseFloat(document.getElementById('storage-cost').innerText);
+    const deliveryCost = parseFloat(document.getElementById('delivery-cost').innerText);
+    const bestPrice = parseFloat(document.getElementById('best-price').innerText);
+    const totalCost = document.getElementById('total-cost');
+    totalCost.innerText = bestPrice + memoryCost + storageCost + deliveryCost;
+    
+    isCouponCodeApplied = false;
+
+    const totalDiscountCost = document.getElementById('discount-total');
+    totalDiscountCost.innerText = parseInt(totalCost.innerText);
+}
+
+
+// Calculate memory cost
+document.getElementById('memory-one').addEventListener('click', function(){
+    productPrice('memory', 0);
+})
+
+document.getElementById('memory-two').addEventListener('click', function(){
+    productPrice('memory', 180);
+})
+
+// Calculate storage cost
+document.getElementById('storage-one').addEventListener('click', function(){
+    productPrice('storage', 0);
+})
+
+document.getElementById('storage-two').addEventListener('click', function(){
+    productPrice('storage', 100);
+})
+document.getElementById('storage-three').addEventListener('click', function(){
+    productPrice('storage', 180);
+})
+
+// Calculate shipping cost
+document.getElementById('delivery-one').addEventListener('click', function(){
+    productPrice('delivery', 0);
+})
+
+document.getElementById('delivery-two').addEventListener('click', function(){
+    productPrice('delivery', 20);
+})
+
+// After discount price
+const couponInput = document.getElementById('coupon-input');
+const couponButton = document.getElementById('coupon-button');
+const totalDiscountCost = document.getElementById('discount-total');
+
+// apply coupon code button event
+couponButton.addEventListener('click', function(){
+    if(couponInput.value == 'stevekaku'){
+        if(!isCouponCodeApplied){
+            const discount = (parseInt(totalDiscountCost.innerText) * 20) / 100;
+            totalDiscountCost.innerText = parseInt(totalDiscountCost.innerText) - discount;
+            couponInput.value = '';
+            isCouponCodeApplied = true;
+        }
+        
+    } else{
+        alert('Please Type Correct Coupon Code!');
+    }
+})
